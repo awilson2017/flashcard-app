82d3268bd9005063fc2294b6b1b490d1657209a9
Angela Wilson <wilsonange@gmail.com>
2018-01-07 10:03:52 -0800

    add paperclip code to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'korean-app',
+      :access_key_id => ENV['AWS_ACCESS_KEY'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
