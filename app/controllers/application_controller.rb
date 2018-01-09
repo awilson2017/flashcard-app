2321f558bda1ba53b9ad00dcebb4dc8ced11f27d
Angela Wilson <wilsonange@gmail.com>
2018-01-09 11:11:51 -0800

    add Oauth code to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+
+  # decodes the JWT token, takes the login, finds user in DB, and then stores it in instance var.
+  def current_user
+    token = params[:token]
+    payload = TokiToki.decode(token)
+    @current_user ||= User.find_by_login(payload[0]['sub'])
+  end
+
+  # boolean of whether or not we can find user in DB
+  def logged_in?
+    current_user != nil
+  end
+
+  # generates a 400's error if we cannot find the user
+  def authenticate_user!
+    head :unauthorized unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   resources :tags do
     get :flashcards, on: :member
   end
+
+  # OAuth Github login route
+  get '/auth/github', to: 'authentication#github', format: false
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
