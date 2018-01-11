9ee3d450dcad0150f7c424188a88d92594bc9110
Angela Wilson <wilsonange@gmail.com>
2018-01-11 13:12:20 -0800

    generate sessions controller and login/logout resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :tags
   resources :flashcards
   resources :users
@@ -10,5 +12,10 @@ Rails.application.routes.draw do
   resources :tags do
     get :flashcards, on: :member
   end
+
+  # login/logout resources
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
